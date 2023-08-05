0448370dbbf6a4b86b6eb45fd21b91f64ef4feeb
Fra <96795244+Alexyo21@users.noreply.github.com>
2023-08-05 17:43:46 +0200

    Add files via upload
    Signed-off-by: Fra <96795244+Alexyo21@users.noreply.github.com>

--- k11_extension/include/types.h
+++ k11_extension/include/types.h
@@ -28,9 +28,15 @@
 
 #pragma once
 
-#include <stdint.h>
+#include <inttypes.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdalign.h>
+#include <unistd.h>
+#include <math.h>
 
 /// The maximum value of a u64.
 #define U64_MAX	UINT64_MAX
