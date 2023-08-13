68d21a55c06cfa075bd685bb4955edb94452082b
Fra <96795244+Alexyo21@users.noreply.github.com>
2023-08-13 17:19:07 +0200

    Delete sysmodules/loader/build directory
    Signed-off-by: Fra <96795244+Alexyo21@users.noreply.github.com>

--- sysmodules/loader/build/romfsredir.d
+++
@@ -1,2 +0,0 @@
-romfsredir.o: \
- /home/ubuntu-budgie/Luma3DS-debug/sysmodules/loader/source/romfsredir.s
--- sysmodules/loader/build/strings.d
+++
@@ -1,6 +0,0 @@
-strings.o: \
- /home/ubuntu-budgie/Luma3DS-debug/sysmodules/loader/source/strings.c \
- /home/ubuntu-budgie/Luma3DS-debug/sysmodules/loader/source/strings.h \
- /opt/devkitpro/libctru/include/3ds/types.h
-/home/ubuntu-budgie/Luma3DS-debug/sysmodules/loader/source/strings.h:
-/opt/devkitpro/libctru/include/3ds/types.h:
