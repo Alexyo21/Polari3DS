8822729960ac98a3438f51e9deef73f8b5ff8a9d
Fra <96795244+Alexyo21@users.noreply.github.com>
2023-08-22 20:11:35 +0200

    Add files via upload
    Signed-off-by: Fra <96795244+Alexyo21@users.noreply.github.com>

--- sysmodules/loader/source/patcher.h
+++ sysmodules/loader/source/patcher.h
@@ -4,6 +4,7 @@
 #include <3ds/exheader.h>
 #include "ifile.h"
 #include "util.h"
+#include "luma_shared_config.h"
 
 #define MAKE_BRANCH(src,dst)      (0xEA000000 | ((u32)((((u8 *)(dst) - (u8 *)(src)) >> 2) - 2) & 0xFFFFFF))
 #define MAKE_BRANCH_LINK(src,dst) (0xEB000000 | ((u32)((((u8 *)(dst) - (u8 *)(src)) >> 2) - 2) & 0xFFFFFF))
