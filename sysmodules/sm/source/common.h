15afe6a265c7350644a7e8a9833349aef687ce02
Fra <96795244+Alexyo21@users.noreply.github.com>
2023-08-22 04:23:33 +0200

    Add files via upload
    Signed-off-by: Fra <96795244+Alexyo21@users.noreply.github.com>

--- sysmodules/sm/source/common.h
+++ sysmodules/sm/source/common.h
@@ -9,6 +9,16 @@ This is part of 3ds_sm, which is licensed under the MIT license (see LICENSE for
 
 #include <3ds.h>
 #include <string.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdalign.h>
+#include <unistd.h>
+#include <math.h>
 
 #define KERNEL_VERSION_MINOR    (GET_VERSION_MINOR(osGetKernelVersion()))
 
