f6e44fd02f2526c3dba0d86ee186b26389b06b9b
Fra <96795244+Alexyo21@users.noreply.github.com>
2023-11-20 11:47:12 +0100

    bump config file version
    Signed-off-by: Fra <96795244+Alexyo21@users.noreply.github.com>

--- arm9/source/config.h
+++ arm9/source/config.h
@@ -38,7 +38,7 @@
 
 #define CONFIG_FILE         "lumae.ini"
 #define CONFIG_VERSIONMAJOR 4
-#define CONFIG_VERSIONMINOR 3
+#define CONFIG_VERSIONMINOR 4
 
 #define BOOTCFG_NAND         BOOTCONFIG(0, 1)
 #define BOOTCFG_EMUINDEX     BOOTCONFIG(1, 3)
