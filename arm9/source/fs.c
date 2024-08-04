5848cd6e02b53a39c1cb33b010d0a3e657b0e181
Fra <96795244+Alexyo21@users.noreply.github.com>
2024-08-04 03:55:56 +0200

    up to latest commit part 2
    Signed-off-by: Fra <96795244+Alexyo21@users.noreply.github.com>

--- arm9/source/fs.c
+++ arm9/source/fs.c
@@ -230,6 +230,12 @@ bool fileCopy(const char *pathSrc, const char *pathDst, bool replace, void *tmpB
     return true;
 }
 
+bool createDir(const char *path)
+{
+    FRESULT res = f_mkdir(path);
+    return res == FR_OK || res == FR_EXIST;
+}
+
 bool findPayload(char *path, u32 pressed)
 {
     const char *pattern;
@@ -500,6 +506,9 @@ bool doLumaUpgradeProcess(void)
     fileDelete("nand:/rw/luma/configExtra.ini");
   //  fileDelete("sdmc:/luma/config.bin");
   //  fileDelete("nand:/rw/luma/config.bin");
+
+    createDir("sdmc:/luma/payloads");
+    createDir("nand:/rw/luma/payloads");
 
     return ok && ok2;
 }
